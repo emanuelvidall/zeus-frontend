499e2813789617b52359f8836ae18c9fd3786d47
Emanuel Vidal <emanuelvidal@unifor.br>
2023-05-19 17:33:20 -0300

    minor styling fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,25 +13,25 @@ const [authenticated, isAuthenticated] = useState(false);
   return (
     <main className='items-center flex justify-center h-screen'>
         <div className='bg-[#1e2229] drop-shadow-xl rounded-xl items-center flex justify-center pl-10 pr-10 h-[90%] w-[90%]'>
-          <div className='bg-white rounded-xl animate__animated animate__bounceInUp w-1/3 h-2/3'>
+          <div className='bg-white rounded-xl animate__animated animate__fadeIn w-1/3 h-2/3'>
             <div className='p-10'>
                 <h1 style={{fontFamily: 'sans-serif'}} className='font-bold color-black text-xl text-center font-readex'>Crie ou Entre com sua conta 🐶</h1>
                 <div className='flex pl-7 justify-center'>
                   <img src='/images/splashlogo1.png' width={250} height={250} alt='logo' />
                 </div>
                 <div className='mt-5 items-center flex flex-col'>
-                  <h1 className='font-bold self-start ml-20'>Login</h1>
+                  <h1 className='font-bold text-sm self-start ml-32'>Login</h1>
                   <form>
-                    <TextField size='small' sx={{ m: 1, width: '25ch' }} id="outlined-basic desc" onChange={()=>{console.log('oi')}} className='' placeholder='utilize seu email' label="" variant="outlined" />
+                    <TextField size='small' sx={{ m: 1, width: '25ch' }} id="outlined-basic desc" onChange={()=>{console.log('oi')}} className='focus:border-blue-500' placeholder='utilize seu email' label="" variant="outlined" />
                   </form>
-                  <h1 className='font-bold self-start ml-20 mt-5'>Senha</h1>
+                  <h1 className='font-bold text-sm self-start ml-32 mt-5'>Senha</h1>
                   <form>
-                    <TextField size='small' sx={{ m: 1, width: '25ch' }} id="outlined-basic desc" onChange={()=>{console.log('oi')}} className='' placeholder='digite sua senha' label="" variant="outlined" />
+                    <TextField size='small' sx={{ m: 1, width: '25ch' }} id="outlined-basic desc" onChange={()=>{console.log('oi')}} className='focus:border-blue-500' placeholder='digite sua senha' label="" variant="outlined" />
                   </form>
                   <h1 className='text-blue-500 text-xs underline hover:cursor-pointer mt-5'>Esqueci minha senha</h1>
                   <div className='flex flex-row mt-10'>
-                    <button className='rounded bg-green-500 hover:scale-110 transition-all duration-300 ease-in-out w-[120px] p-2 mr-5 font-bold text-white'>Login</button>
-                    <button className='rounded bg-blue-500 hover:scale-110 transition-all duration-300 ease-in-out w-[120px] p-2 ml-5 font-bold text-white'>Registrar</button>
+                    <button className='rounded bg-green-500 w-[120px] h-[30px] mr-5 text-white'>Login</button>
+                    <button className='rounded bg-blue-500 w-[120px] h-[30px] ml-5 text-white'>Registrar</button>
                   </div>
                 </div>
             </div>
